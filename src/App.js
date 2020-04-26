85f0acb6ed4da1125c06f933d13b44a7168ef862
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-04-26 20:29:01 +0530

    refractor and readme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Draggable from './Draggable';
 
-class App extends Component{
+class App extends Component {
   state = {
-    comp : null,
-    num:0,
-    arr:[]
+    comp: null,
+    num: 0,
+    arr: []
   };
 
-  getComp = () =>{
-    const {Comp, num} = this.state;
+  getComp = () => {
+    const { Comp, num } = this.state;
 
     let _arr = [...this.state.arr]
 
-    if (_arr.length > 0 ){
+    if (_arr.length > 0) {
       let h = _arr[num - 1].h + 150;
       let w = _arr[num - 1].w + 150;
       let bg = `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
@@ -25,46 +24,46 @@ class App extends Component{
         bg
       });
     }
-    else{
+    else {
       let h = 100,
-      w = 100,
-      bg = `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
+        w = 100,
+        bg = `#${((Math.random() * 0xffffff) << 0).toString(16)}`;
       _arr = [{
         h,
-        w, 
+        w,
         bg
       }];
     }
-    
-    
+
+
     this.setState({
       arr: _arr,
-      num: num +1
+      num: num + 1
     })
 
-    let index = num -1 > 0? num -1 : 0;
+    let index = num - 1 > 0 ? num - 1 : 0;
 
-    return(
+    return (
       <Draggable
-      height={`${_arr[index].h}px`}
-      width={`${_arr[index].w}px`}
-      bg={`${_arr[index].bg}`}
-    >
-      {Comp && Comp}
+        height={`${_arr[index].h}px`}
+        width={`${_arr[index].w}px`}
+        bg={`${_arr[index].bg}`}
+      >
+        {Comp && Comp}
       </Draggable>
     );
 
 
   }
 
-  add =() =>{
+  add = () => {
     let Comp = this.getComp();
     this.setState({
       Comp
     })
   }
 
-  render = () =>{
+  render = () => {
     return (
       <div>
         <button onClick={this.add}> Add Parent </button>
--- src/Draggable.js
+++ src/Draggable.js
@@ -1,28 +1,28 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Draggable from "react-draggable"
 
-export default class Drag extends Component{
-    render = () =>{
-        return(
+export default class Drag extends Component {
+    render = () => {
+        return (
             <div className="container">
                 <Draggable bounds="parent">
                     <div
                         style={{
-                            width:this.props.width || "200px",
-                            border : "1px solid red"
+                            width: this.props.width || "200px",
+                            border: "1px solid red"
                         }}
                         className="draggable"
+                    >
+                        <div style={{ background: "yellow" }}> Title</div>
+                        <div
+                            style={{
+                                height: this.props.height || "30 px",
+                                background: this.props.bg || "red"
+                            }}
                         >
-                            <div style={{background:"yellow"}}> Title</div>
-                            <div
-                                style={{
-                                    height: this.props.height || "30 px",
-                                    background : this.props.bg || "red"
-                                }}
-                                >
-                                 {this.props.children && this.props.children}   
-                                </div>
+                            {this.props.children && this.props.children}
                         </div>
+                    </div>
                 </Draggable>
 
             </div>
